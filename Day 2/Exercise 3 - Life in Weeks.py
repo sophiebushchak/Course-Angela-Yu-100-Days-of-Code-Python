8ed4dc6e0370baf8a82718ae298aaf7bf6704ac9
Sophie Bushchak <sophie.bushchak@hva.nl>
2022-01-22 15:57:35 +0100

    Day 2 - Exercise 3

---
+++ Day 2/Exercise 3 - Life in Weeks.py
@@ -0,0 +1,7 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?\n")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+years_left = 90 - int(age)
+print(f'You have {years_left * 365} days, {years_left * 52} weeks, and {years_left * 12} months left.')
